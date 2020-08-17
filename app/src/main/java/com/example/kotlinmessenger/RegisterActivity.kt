53905fb21d1324642db1e7fb3fafb9f404214491
shenglimdev <69244497+shenglimdev@users.noreply.github.com>
2020-08-17 17:39:29 +1000

    initialise login page

--- app/src/main/java/com/example/kotlinmessenger/RegisterActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/RegisterActivity.kt
@@ -1,12 +1,24 @@
 package com.example.kotlinmessenger
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_register)
+
+        register_button_register.setOnClickListener {
+            val email = email_editTextTextEmailAddress_register.text.toString()
+            val password = password_editTextTextPassword_register.text.toString()
+        }
+
+        already_have_account_textView3_register.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
