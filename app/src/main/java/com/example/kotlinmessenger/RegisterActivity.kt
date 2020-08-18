5bdda01a99c5cc652e599a071b40e7ef79f1d04b
shenglimdev <69244497+shenglimdev@users.noreply.github.com>
2020-08-18 17:36:59 +1000

    succesfully created a test user from within app

--- app/src/main/java/com/example/kotlinmessenger/RegisterActivity.kt
+++ app/src/main/java/com/example/kotlinmessenger/RegisterActivity.kt
@@ -2,7 +2,10 @@ package com.example.kotlinmessenger
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -14,6 +17,22 @@ class RegisterActivity : AppCompatActivity() {
         register_button_register.setOnClickListener {
             val email = email_editTextTextEmailAddress_register.text.toString()
             val password = password_editTextTextPassword_register.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+
+                    //else if successful
+                    Log.d("Register", "Successfully created user with uid: ${it.result?.user?.uid}")
+                }
+                .addOnFailureListener {
+                    Log.d("Register", "Failed to create user: ${it.message}")
+                }
         }
 
         back_to_registration_textView_login.setOnClickListener {
